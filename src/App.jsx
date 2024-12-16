e2726c01938515d36ec9d1f0d080b2b8bfcc7997
Ronee <137155067+RONHEL235@users.noreply.github.com>
2024-12-16 21:32:24 +0200

    Introduced an index.js file so as to avoid writing many import statements in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styles from "./style";
 
+import Navbar from "./components/Navbar"
+
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
